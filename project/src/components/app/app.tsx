f85ed563fa90ca6f3f080c779c7549c3687a1bfd
alexander-morozov-travelline <morozov.alexvitvvv@gmail.com>
2022-02-23 23:52:00 +0300

    3.5. Маршрут по тайным тропам https://up.htmlacademy.ru/react/9/tasks/4

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
-import MainPage from '../main-page/main-page';
+import MainPage from '../../pages/main-page/main-page';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import React from 'react';
+import Layout from '../layout/layout';
+import Property from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
+import Login from '../../pages/login/login';
+import PrivateRoute from '../private-route/private-route';
+import Favorites from '../../pages/favorites/favorites';
 
 type AppProps = {
   countAds: number;
@@ -6,7 +15,22 @@ type AppProps = {
 function App(props: AppProps): JSX.Element {
   const {countAds} = props;
   return (
-    <MainPage countAds={countAds}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Layout />}>
+          <Route index element={<MainPage countAds={countAds} />} />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Offer} element={<Property />} />
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+          />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
