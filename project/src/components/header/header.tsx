454148671cb39425de93ae2ecadbd29dd4235a32
alexander_morozov-tl <alexander.morozov@travelline.ru>
2022-03-18 17:27:55 +0300

    7.10. Истина где-то на сервере https://up.htmlacademy.ru/react/9/tasks/10

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -4,11 +4,7 @@ import SignOut from '../sign-out/sign-out';
 import {useAppSelector} from '../../hooks/hooks';
 import {State} from '../../types/state';
 
-type HeaderProps = {
-  isLoginPage?: boolean
-};
-
-function Header({isLoginPage = false}: HeaderProps): JSX.Element {
+function Header(): JSX.Element {
   const {authorizationStatus} = useAppSelector((state: State) => state);
   return (
     <header className="header">
@@ -19,32 +15,27 @@ function Header({isLoginPage = false}: HeaderProps): JSX.Element {
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
             </Link>
           </div>
-          {
-            !isLoginPage
-              ?
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      {
-                        authorizationStatus === AuthorizationStatus.Auth
-                          ? <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                          : <span className="header__login">Sign in</span>
-                      }
-                    </Link>
-                  </li>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
                   {
-                    (authorizationStatus === AuthorizationStatus.Auth)
-                      ?
-                      <SignOut />
-                      : null
+                    authorizationStatus === AuthorizationStatus.Auth
+                      ? <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      : <span className="header__login">Sign in</span>
                   }
-                </ul>
-              </nav>
-              : null
-          }
+                </Link>
+              </li>
+              {
+                (authorizationStatus === AuthorizationStatus.Auth)
+                  ?
+                  <SignOut />
+                  : null
+              }
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
