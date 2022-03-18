fbd078399096e82423fb66091058607813a183cb
alexander_morozov-tl <alexander.morozov@travelline.ru>
2022-03-18 10:05:59 +0300

    7.10. Истина где-то на сервере https://up.htmlacademy.ru/react/9/tasks/10

--- project/src/components/sign-out/sign-out.tsx
+++ project/src/components/sign-out/sign-out.tsx
@@ -1,18 +1,19 @@
-import {FormEvent} from 'react';
-import {logoutAction} from '../../store/api-actions';
 import {useAppDispatch} from '../../hooks/hooks';
+import {MouseEvent} from 'react';
+import {logoutAction} from '../../store/api-actions';
 
 function SignOut():JSX.Element {
   const dispatch = useAppDispatch();
 
-  const handleSignOut = (evt: FormEvent<HTMLFormElement>) => {
+  const handleSignOut = (evt: MouseEvent<HTMLElement>) => {
     evt.preventDefault();
-    dispatch(logoutAction);
+    dispatch(logoutAction());
   };
+
   return (
     <li className="header__nav-item">
-      <a className="header__nav-link" href="#">
-        <span className="header__signout" onClick={() => handleSignOut}>Sign out</span>
+      <a className="header__nav-link" href="#" onClick={handleSignOut}>
+        <span className="header__signout">Sign out</span>
       </a>
     </li>
   );
